fb9ff64120b7a7621767911169bb9d61f0435869
JoshuaPierson <Josh@Joshuajackpierson.com>
2013-07-02 00:06:37 +0100

    Finish sign in

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name     "Joshua Pierson"
-    email    "Josh@example.com"
+    name     "Example User"
+    email    "user@example.com"
     password "foobar"
     password_confirmation "foobar"
   end
