e6babacc027ae7584717bbed26d7bafb83d7862f
JoshuaPierson <Josh@Joshuajackpierson.com>
2013-07-08 16:06:17 +0100

    upload for view

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,7 @@ module SessionsHelper
 
 	def sign_in(user)
     	cookies.permanent[:remember_token] = user.remember_token
-    	self.current_user = user
+    	@current_user = user
     end
     
     def signed_in?
@@ -14,11 +14,12 @@ module SessionsHelper
     end
     
     def current_user
+    #if cookies[:remember_token].present? ( To sign out of heroku)
     	@current_user ||= User.find_by_remember_token(cookies[:remember_token])
     end
     
     def sign_out
-    	self.current_user = nil 
+    	@current_user = nil 
     	cookies.delete(:remember_token)
     end
    end
